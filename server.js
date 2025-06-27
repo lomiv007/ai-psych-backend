7fc8df5f0650e805e90b3552c16334b0f0517f21
lomiv007 <vukovichosting@gmail.com>
2025-06-27 14:40:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import OpenAI from "openai";
+
+
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import { OpenAI } from 'openai';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
